fd6d141ed12d5a8eb3b2c78f75bd989d8c49b2c5
MajesticPotatoe <amajesticpotatoe@gmail.com>
2020-01-24 19:10:00 -0500

    docs: migrate bootstrap options

--- packages/docs-cli/src/plugins/vuetify.js
+++ packages/docs-cli/src/plugins/vuetify.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import goTo from 'vuetify/lib/services/goto'
 import 'vuetify/dist/vuetify.css'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#1867C0',
+        secondary: '#5CBBF6',
+        tertiary: '#E57373',
+        accent: '#005CAF',
+      },
+    },
+  },
+})
+
+goTo.framework = vuetify.framework
+
+export default vuetify
